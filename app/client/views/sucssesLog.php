981f6b403f4ee6dd162f53dac99dda83378e4103
Samuel Seven <sevenofice777@gmail.com>
2023-11-05 18:40:52 -0300

    Reorganizei para PADRÃO MVC

--- app/client/views/sucssesLog.php
+++ app/client/views/sucssesLog.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>7TECH - Carregando</title>
   <link rel="shortcut icon" href="../img-all/logo_crud.png" type="image/x-icon">
-  <link rel="stylesheet" type="text/css" href="../css/logs.css">
+  <link rel="stylesheet" type="text/css" href="../css/style.css">
 
 </head>
 
