453d38b6e5f22108fadcf86649ee8e44729b21c8
Samuel <sevenofice777@gmail.com>
2023-11-09 14:29:07 -0200

    UPDATE 1.6.1 - Criando pagina do usuario e logica

--- app/client/views/7tech-company/index.php
+++ app/client/views/7tech-company/index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> 7TECH - <?php echo $_SESSION["NOME"] ?> </title>
+    <title> 7TECH - BEM-VINDO <?php echo $_SESSION["NOME"] ?> </title>
     
     <!-- css -->
     <link rel="stylesheet" href="../../styles/style.css">
@@ -14,6 +14,10 @@
     <link rel="stylesheet" media="screen and (max-width: 950px)" href="../../styles/responsive/styles-responsive.css">
     <!-- favicon -->
     <link rel="shortcut icon" href="../../assets/img/logo_crud.png" type="image/x-icon">
+
+    <!-- ion-icons -->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
 <body>
     <header>
@@ -21,13 +25,24 @@
             <img src="../../assets/img/logo_crud.png" alt="Logo">
         </div>
         <div class="user-picture">
-            <span name="username">
-                <?php 
-                   echo $_SESSION["NOME"];
-                ?>
-            </span>
-            <img src="#" name="user-picture" alt="user-pictuer">
+            <div class="user-picture user-data">
+                <span name="username">
+                    <?php
+                       echo $_SESSION["NOME"];
+                    ?>
+                </span>
+                <img src="../../assets/img/user-picture.png" name="user-picture" alt="user-pictuer">
+            </div>
+                <ion-icon name="log-out-outline" id="logout-icon">
+                    <figcaption>SAIR</figcaption>
+                </ion-icon>
         </div>
     </header>
+
+    <div class="container user-area-screen">
+        <div class="start_website">
+
+        </div>
+    </div>
 </body>
 </html>
