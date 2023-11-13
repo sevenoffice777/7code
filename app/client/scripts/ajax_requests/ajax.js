aff804c5664aedf2fef98210d3373edcb8be9a04
Samuel Seven <sevenofice777@gmail.com>
2023-11-13 10:49:08 -0300

    Update 1,6.6.1 - Funcionalizando as requests AJAX
    Colocando A funcionalidade AjaxRequests para versatilizar mais o codigo

--- app/client/scripts/ajax_requests/ajax.js
+++ app/client/scripts/ajax_requests/ajax.js
@@ -56,29 +56,28 @@ $('[btn-sbmt]').each(function () {
 
         // Ajax --> Request Sever-side and client-side
         //Ajax --> url, method, cache, dataType, contentType, processData, data, --> sucsses, error
-        $.ajax({
-            url: parent.attr('dataForm'),   // URL para onde a requisição será enviada, obtida do atributo 'dataForm' do elemento 'parent'
-            method: 'POST',                  // Método HTTP a ser usado (neste caso, POST)
-            cache: false,                    // Define se o resultado da requisição deve ser armazenado em cache pelo navegador (não cacheado neste caso)
-            dataType: 'json',                // Tipo de dados esperado na resposta (JSON neste caso)
-            contentType: false,              // Tipo de conteúdo da requisição (definido como falso para permitir que o navegador defina automaticamente com base nos dados)
-            processData: false,               // Define se os dados fornecidos na propriedade 'data' devem ser processados automaticamente (definido como falso para enviar FormData)
-            data: formData,                  // Dados a serem enviados com a requisição (FormData neste caso, que contém os dados do formulário)
-            success: function (response) {   // Função a ser chamada quando a requisição é bem-sucedida
+       
+
+        ajaxRequest(
+            parent.attr('dataForm'),
+            "POST",
+            'json',
+            formData,
+            function (response) {
                 console.log(response);       // Exibe a resposta no console
                 if (response.url) {          // Verifica se a resposta possui uma propriedade 'url'
                     window.location.href = response.url;  // Redireciona para a URL fornecida na resposta
                 } else {
                     log_error.html(response.msg_erro);    // Exibe a mensagem de erro no elemento com a classe 'log_error'
                 }
-                enableOrDisabledButton(false, btnSbmt, 'Cadastrar');  // Desabilita o botão de envio
+                enableOrDisabledButton(false, btnSbmt, 'Cadastrar');
             },
-            error: function (xhr, statusServer, errorName) {   // Função a ser chamada em caso de erro na requisição
-                console.log(`${xhr} - ${statusServer}, ${errorName}`);  // Exibe informações do erro no console
+            function () {
+                console.log(`${xhr} - ${statusServer}, ${errorName}`);
             }
-        });
-        
-      
+        )
+
+
 
     })
 }) // --> e a mesma coisa que o document.querySelector........
