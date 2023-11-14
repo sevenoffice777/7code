c5ccd0c3b5ad6ae2abe173cf17f31c01e9563d22
Marckson <marcksonasantos@gmail.com>
2023-11-13 23:37:01 -0300

    Update 1.7 - Atualizando o Sql, loginUser

--- app/client/views/signin.php
+++ app/client/views/signin.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 
+<?php
+session_start();
+
+var_dump($_SESSION);
+?>
+
 <head>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
