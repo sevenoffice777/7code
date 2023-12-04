15e07fe9e9349001f8b2d8b3c603200667729e65
Samuel Seven <sevenofice777@gmail.com>
2023-12-04 10:52:23 -0300

    feat(chatbot): refatored code - clean code

--- app/client/scripts/functions/chatbot.js
+++ app/client/scripts/functions/chatbot.js
@@ -1,64 +1,90 @@
 let iconSubmitMessage = document.querySelector('#send-icon');
 let assuntoChatbot = '';
 let responseUser = '';
+let inputChatbot = document.querySelector('#input-chatbot');
+
 iconSubmitMessage.addEventListener('click', () => {
+    insertIntoChatbot();
+})
 
+inputChatbot.onkeydown = (e) => {
+    if(e.code == 'Enter') {
+        insertIntoChatbot();
+    }
+}
+
+function insertIntoChatbot() {
     let container_content_chatbot = document.querySelector('.container-content-chatbot');
-    let inputChatbot = document.querySelector('#input-chatbot');
-
-
 
     if (!inputChatbot.value.trim() == '') {
         // trim() --> função que verifica se uma string esta vazia!
         let messageElementQuestion = document.createElement('div');
         let message = inputChatbot.value;
         messageElementQuestion.innerHTML = `<p class="message-question">${message}</p>`;
-        container_content_chatbot.appendChild(messageElementQuestion)
+        container_content_chatbot.appendChild(messageElementQuestion);
         inputChatbot.value = "";
         setTimeout(() => {
             let messageElementResponse = document.createElement('div');
             messageElementResponse.innerHTML = `<p class="message-response">${IAresponse(message)}</p>`;
             container_content_chatbot.appendChild(messageElementResponse);
+            container_content_chatbot.scrollTop = container_content_chatbot.scrollHeight;
         }, 500);
 
 
     }
-
-})
+}
 
 
 function IAresponse(message) {
     let responseIA = '';
-
-    if (/oi/i.test(message)) {
-        responseIA = 'Oii, como posso ajudar?';
-    } else if (/qual seu nome/i.test(message) || /como você chama?/i.test(message)) {
-        responseIA = 'Sou uma IA desenvolvida por &copy; Samuel Seven, estou aqui para te ajudar!';
-    } else if (/ajuda/i.test(message) || /preciso de ajuda/i.test(message)) {
-        responseIA = 'Pode me perguntar alguma coisa ou precisa de algo específico?';
-    } else if (/extrato/i.test(message)) {
-        responseIA = 'Para obter o extrato da sua conta, você pode acessar o internet banking ou usar um caixa eletrônico. Posso te ajudar a encontrar a opção certa?';
-    } else if (/transferência/i.test(message) || /transferir/i.test(message)) {
-        responseIA = 'Para fazer uma transferência, você pode usar o aplicativo móvel, o internet banking ou um caixa eletrônico. Se precisar de assistência, estou à disposição!';
-    } else if (/cartão de crédito/i.test(message)) {
-        responseIA = 'Para informações sobre seu cartão de crédito, como fatura e limite, recomendo verificar no internet banking ou entrar em contato com nosso suporte ao cliente.';
-    } else if (/pagamento/i.test(message) || /conta/i.test(message)) {
-        responseIA = 'Você pode pagar suas contas usando o aplicativo móvel, o internet banking ou um caixa eletrônico. Posso fornecer mais detalhes sobre como fazer isso?';
-    } else if (/como alterar senha/i.test(message)) {
-        responseIA = 'Para alterar sua senha, acesse o internet banking e procure a opção de configurações de segurança. Se precisar de assistência, estou aqui para ajudar!';
-    } else if (/bloquear cartão/i.test(message)) {
+    // ||
+    if ((/oi/i.test(message)) || /eai/i.test(message) || /ola/i.test(message) || /oie/i.test(message)) {
+        responseIA = 'Olá! Tudo bem? Em que posso ser útil hoje?';
+    }
+    // Consulta de extrato
+    else if (searchInMessage('extrato', message)) {
+        responseIA = `Se quiser saber mais sobre extratos, clique na opção EXTRATO. Lá, você pode fazer consultas das suas últimas compras por data, valor, entre outros...`;
+    }
+    // Pergunta sobre pagamento de contas
+    else if (searchInMessage('pagamento', message) || searchInMessage('conta', message)) {
+        responseIA = 'Para efetuar o pagamento de contas, você pode usar o aplicativo móvel, internet banking ou um caixa eletrônico. Posso te ajudar com isso?';
+    }
+    // Pergunta sobre transferência
+    else if (searchInMessage('transferência', message) || searchInMessage('transferir', message)) {
+        responseIA = 'Para fazer uma transferência, você pode utilizar o aplicativo móvel, internet banking ou um caixa eletrônico. Precisa de ajuda com alguma dessas opções?';
+    }
+    // Pergunta sobre bloqueio de cartão
+    else if (searchInMessage('bloquear cartão', message)) {
         responseIA = 'Se precisar bloquear seu cartão, entre em contato com nosso suporte ao cliente imediatamente. Eles poderão ajudá-lo a tomar as medidas necessárias.';
-    } else if (/agência/i.test(message)) {
+    }
+    // Pergunta sobre agência bancária
+    else if (searchInMessage('agência', message)) {
         responseIA = 'A agência bancária mais próxima de você pode ser encontrada no nosso site ou no aplicativo móvel. Posso fornecer mais informações ou ajudar com algo mais?';
-    } else if (/cheque/i.test(message)) {
-        responseIA = 'Para informações sobre cheques, você pode entrar em contato com nossa agência ou verificar o internet banking. Posso te ajudar com mais alguma coisa?';
-    } else if (/investimentos/i.test(message) || /como investir/i.test(message)) {
+    }
+    // Pergunta sobre investimentos
+    else if (searchInMessage('investimentos', message) || searchInMessage('como investir', message)) {
         responseIA = 'Para obter informações sobre investimentos e como começar, você pode agendar uma consulta com um de nossos consultores financeiros ou verificar as opções disponíveis no internet banking.';
-    } else if (/empréstimo/i.test(message) || /como obter um empréstimo/i.test(message)) {
+    }
+    // Pergunta sobre empréstimo
+    else if (searchInMessage('empréstimo', message) || searchInMessage('como obter um empréstimo', message)) {
         responseIA = 'Se você estiver interessado em obter um empréstimo, recomendo entrar em contato com nossa equipe de empréstimos. Eles poderão orientá-lo sobre os requisitos e opções disponíveis.';
-    } else {
+    }
+    else if(searchInMessage('saldo', message)) {
+        responseIA = 'Pra descobrir seu saldo clique na sua foto no canto SUPERIOR DIREITO DA TELA';
+    }
+    // Caso nenhuma correspondência seja encontrada
+    else {
         responseIA = 'Desculpe, não entendi completamente. Pode repetir ou fornecer mais detalhes, por favor?';
     }
-
+        
+    
     return responseIA;
+}
+
+function searchInMessage(palavrachave, messageUser) {
+    let regex = new RegExp("\\b" + palavrachave + "\\b", "i");
+
+    const res = regex.test(messageUser);
+
+    return res;
 }
