5e3bdcf7194d2fd5d19e79ec30bd9bada27a8aa1
Samuel Seven <sevenofice777@gmail.com>
2024-01-25 23:58:07 -0300

    feat: Transferencia realizada

--- app/server/models/transferenciasMenu.php
+++ app/server/models/transferenciasMenu.php
@@ -28,18 +28,44 @@ $saldoUser_nFormatado = prepareAndExecute($conn, "SELECT saldo FROM bankaccount 
 
 if ($saldoUser_nFormatado["saldo"] == "0.00") {
     $saldoFormatado = 0.00;
+
+    $jsonData["resposta"] = "undefinedValue";
+    $jsonData["msg"] = "Saldo Indisponivel!";
 } else {
     $saldoFormatado = converteValorParaReal($saldoUser_nFormatado["saldo"]);
-    // $jsonData["resposta"] = $saldoFormatado;
-    if ($valorTransferencia >= 0) {
+    if ($valorTransferencia > 0) {
+
         // Verificar se o usuario tem saldo suficiente para realizar a transfer
         if ($valorTransferencia > $saldoFormatado) {
-            echo json_encode("Saldo Insuficiente");
-        } else {
-            $jsonData["resposta"] = "Transação Realizada Com sucesso";
-        }
+            $jsonData["resposta"] = "undefinedValue";
+            $jsonData["msg"] = "Saldo Insuficiente";
 
+        } else {
+            // Saldo QUe o usuario tera apos a transação!
+            $saldoPosTransferencia = $saldoFormatado - $valorTransferencia;
+            //Passo 1 --> Verificar se o account_id_destiny existe
+            $consultaContaDestino = prepareAndExecute($conn, "SELECT * FROM bankaccount WHERE account_id = ?", array($dados["account_id_destiny"]), 's', 'selectOne');
+            $jsonData["resposta"] = $consultaContaDestino["account_id"];
+            // if ($consulaContaDestino) {
+            //     // Passo 2 --> Retirar o valor da conta do cliente... ( saldoFormatado - valorTransferencia )
+            //     prepareAndExecute($conn, "UPDATE bankaccount SET saldo = ? WHERE account_id = ?", array($saldoPosTransferencia, $_SESSION["account_id"]), 'ii', "upt-update");
+
+            //     // Passo 3 --> Inserir o valorTransferencia no saldo do usuario final (contaDestino)... 
+
+
+
+            //     $jsonData["resposta"] = "successTransaction";
+            //     $jsonData["msg"] = "SUCESSO";
+            // } else {
+            //     $jsonData["resposta"] = "undefinedValue";
+            //     $jsonData["msg"] = "Conta de Destino não Encontrada!";
+            // }
+        }
+    } else {
+        $jsonData["resposta"] = "undefinedValue";
+        $jsonData["msg"] = "Valor Para Transferencia Inválido!";
     }
+
 }
 
 
